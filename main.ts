ed1bbfece6c681fc81f98eb7a728900858d7e601
jendaaaaa <143932605+jendaaaaa@users.noreply.github.com>
2024-04-19 17:52:12 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,37 +1,66 @@
-let up = 0
-let down = 0
-let rotation = 0
-let horizontal = 0
-let winch_inner = 0
-let winch_outer = 0
-let gripper = 0
-let VERTICAL_SPEED = 30
-let ROTATION_SPEED = 30
+led.enable(false)
+let HORIZONTAL_SPEED = 70
 let WINCH_SPEED = 70
-let PRESSED = 1
-let NOT_PRESSED = 0
+let ROTATION_SPEED = 20
+let PRESSED = 0
+let RELEASED = 1
+let SERVO_STALL = 90
+let spool_outer = 0
+let spool_inner = 0
+let gripper = 0
+let rotor = 0
+let hoist = 0
+let left = 0
+let right = 0
+let forward = 0
+let backward = 0
+radio.setGroup(11)
+pins.setPull(DigitalPin.P13, PinPullMode.PullUp)
 pins.setPull(DigitalPin.P14, PinPullMode.PullUp)
 pins.setPull(DigitalPin.P15, PinPullMode.PullUp)
 pins.setPull(DigitalPin.P16, PinPullMode.PullUp)
-radio.setGroup(120)
 basic.forever(function () {
-    horizontal = pins.analogReadPin(AnalogPin.P1)
-    rotation = 1024 - pins.analogReadPin(AnalogPin.P2)
-    up = pins.digitalReadPin(DigitalPin.P14)
-    down = pins.digitalReadPin(DigitalPin.P16)
-    gripper = pins.digitalReadPin(DigitalPin.P15)
-    if (up == PRESSED && down == NOT_PRESSED) {
-        winch_inner = VERTICAL_SPEED
-        winch_outer = VERTICAL_SPEED
-    } else if (up == NOT_PRESSED && down == PRESSED) {
-        winch_inner = -VERTICAL_SPEED
-        winch_outer = -VERTICAL_SPEED
+    hoist = 1024 - pins.analogReadPin(AnalogPin.P1)
+    left = pins.digitalReadPin(DigitalPin.P13)
+    right = pins.digitalReadPin(DigitalPin.P15)
+    forward = pins.digitalReadPin(DigitalPin.P14)
+    backward = pins.digitalReadPin(DigitalPin.P16)
+    if (left == PRESSED && right == PRESSED) {
+        rotor = 0
+    } else if (left == RELEASED && right == PRESSED) {
+        rotor = + ROTATION_SPEED
+    } else if (left == PRESSED && right == RELEASED) {
+        rotor = - ROTATION_SPEED
     } else {
-        winch_inner = Math.map(1024 - horizontal, 0, 1023, -WINCH_SPEED, WINCH_SPEED)
-        winch_outer = Math.map(horizontal, 0, 1023, -WINCH_SPEED, WINCH_SPEED)
+        rotor = 0
     }
-    radio.sendValue("rotation", Math.map(rotation, 0, 1023, -ROTATION_SPEED, ROTATION_SPEED))
-    radio.sendValue("inner", winch_inner)
-    radio.sendValue("outer", winch_outer)
+    if (forward == PRESSED && backward == RELEASED) {
+        spool_inner = + HORIZONTAL_SPEED
+        spool_outer = - HORIZONTAL_SPEED
+    } else if (forward == RELEASED && backward == PRESSED) {
+        spool_inner = - HORIZONTAL_SPEED
+        spool_outer = + HORIZONTAL_SPEED
+    } else if (forward == RELEASED && backward == RELEASED) {
+        spool_inner = Math.map(hoist, 0, 1023, - WINCH_SPEED, WINCH_SPEED)
+        spool_outer = spool_inner
+    } else {
+        spool_inner = 0
+        spool_outer = spool_inner
+    }
+    radio.sendValue("rotate", rotor)
+    radio.sendValue("inner", spool_inner)
+    radio.sendValue("outer", spool_outer)
     radio.sendValue("gripper", gripper)
+})
+
+control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_A, EventBusValue.MICROBIT_EVT_ANY, function () {
+    if (control.eventValue() == EventBusValue.MICROBIT_BUTTON_EVT_DOWN) {
+        gripper = 1
+    }
+})
+
+control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_B, EventBusValue.MICROBIT_EVT_ANY, function () {
+    if (control.eventValue() == EventBusValue.MICROBIT_BUTTON_EVT_DOWN) {
+        gripper = 0
+    }
 })
